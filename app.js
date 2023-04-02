396019817d15e6afde3c0a102b194dbe22d23028
@NickLi0304 <twinpeterlee@gmail.com>
2023-04-02 23:50:29 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const Port = 3000;
+
+// 設定首頁路由
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+// 設定 port 3000
+app.listen(Port, () => {
+  console.log("App is running on http://localhost:3000");
+});
