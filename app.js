af7d59c2e4fac7187e74ff62c3ac1ab3579aa511
@NickLi0304 <twinpeterlee@gmail.com>
2023-04-03 01:14:45 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+//app
 const app = express();
 const Port = 3000;
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+// 連線異常
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+// 連線成功
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
 
 // 設定首頁路由
 app.get("/", (req, res) => {
