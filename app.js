5c2ed30dd40492dba4c0977a80643a277c35891c
@NickLi0304 <twinpeterlee@gmail.com>
2023-04-04 01:12:06 +0800

    add shorten function & routes

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const mongoose = require("mongoose");
-
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+const bodyParser = require("body-parser");
+const routes = require("./routes");
+require("./config/mongoose");
 
 //app
 const app = express();
@@ -13,29 +11,10 @@ const Port = 3000;
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-// 連線異常
-db.on("error", () => {
-  console.log("mongodb error!");
-});
-// 連線成功
-db.once("open", () => {
-  console.log("mongodb connected!");
-});
-
-// 設定首頁路由
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-// 設定 port 3000
+// 設定 port
 app.listen(Port, () => {
-  console.log("App is running on http://localhost:3000");
+  console.log(`App is running on http://localhost:${Port}`);
 });
